cb594fea0043633d10f47d3abaecbeeba25439ea
onecashdev <onecashdev@onecash.finance>
2021-01-16 22:45:37 +0800

    3% app store fund

--- contracts/SimpleERCFund.sol
+++ contracts/SimpleERCFund.sol
@@ -2,12 +2,14 @@ pragma solidity ^0.6.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
 
 import './owner/Operator.sol';
 import './interfaces/ISimpleERCFund.sol';
 
 contract SimpleERCFund is ISimpleERCFund, Operator {
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
     function deposit(
         address token,
@@ -15,6 +17,12 @@ contract SimpleERCFund is ISimpleERCFund, Operator {
         string memory reason
     ) public override {
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+        address devFundAddr = 0xC7da206a87d85e3f9FF06c761E540553c648bC19;
+        address appStoreFundAddr = 0x03BB6e3aa1524720bCf6c010e8c67C2ACe7522B2;
+        IERC20(token).safeTransfer(devFundAddr, amount.mul(2).div(5));      // 2% for dev
+        IERC20(token).safeTransfer(appStoreFundAddr, amount.mul(3).div(5)); // 3% for app store operation
+        
         emit Deposit(msg.sender, now, reason);
     }
 
